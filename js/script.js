e4b4bef385bcedaca5befa1ed5759ec2bafb9a3a
Vladislav Knyazev <tarq138@gmail.com>
2018-09-29 13:37:05 +0300

    nexttry

--- js/script.js
+++ js/script.js
@@ -18,14 +18,33 @@ var InfoX = document.getElementById('X');
 function draw() {
     var rover = document.getElementById("rover");
     document.getElementById('A').onclick = function(){ 
-        XNow += UNow;
-        UNow *= 2;
-        
-        if ((xc > 200) && (UNow>1)) {
-            xc = 200;
-        }
-        if ((xc < 800) && (UNow<1)) {
-            xc = 800;
+        if (Math.abs(XNow + UNow)>100000){
+            alert("Вы достигли границы");
+            XNow=0;
+            UNow=1;
+            MN=1;
+            rover.src = "img/roverRight.png";
+            xc=500;
+        } else{
+            if (((xc + step*UNow)>RoverWidht/2/MN)&&((xc + step*UNow)< (1000-RoverWidht/2/MN))){
+                xc += step*UNow;
+            } else{
+                if (UNow>0){
+                    if (MN <3){
+                        MN++;
+                    }
+                    xc = 200/MN;
+                }
+                if (UNow<0){
+                    if (MN <3){
+                        MN++;
+                    }
+                    xc = 1000 - (2 * RoverWidht/2/MN);
+                }
+
+            }
+            XNow += UNow;
+            UNow *= 2;
         }
         requestAnimationFrame(draw);
     } 
@@ -42,35 +61,23 @@ function draw() {
     }
     ctx.fillStyle = '#000';
     ctx.fillRect(0,0,MaxX,MaxY);
-    step = MaxX / (10 * MN);
+    if (MN < 3) {
+        step = MaxX / (10*MN);
+        FontSize =30 / MN;
+    }
     ctx.strokeStyle = '#fff';
     ctx.beginPath();
     ctx.moveTo(0, MaxY / 2);
     ctx.lineTo(MaxX, MaxY / 2);
-    ctx.lineWidth = 4 / MN;
+    ctx.lineWidth = 4 ;
     ctx.stroke();
     
-        if (UNow > 0){
-            while (Math.abs(MaxX - xc) <= (Math.abs(UNow)*step)){
-                MN++;
-                if (MN < 4) {xc = xc / MN;}
-                step = MaxX / (10 * MN);
-            }
-        }
-        if (UNow < 0){
-            while (xc <= (Math.abs(UNow)*step)){
-                MN++;
-                xc = 800 + 200-200/(MN - 1);
-                step = MaxX / (10 * MN);
-            }
-        }
-    if (MN<=3) {FontSize = 30/ MN;}
     ctx.font = FontSize.toString() + "px Arial";
     ctx.fillStyle = '#fff';
     ctx.fillText(XNow,xc,yc + RoverHeight/1.5/MN);
     ctx.beginPath();
-    ctx.moveTo(xc, yc + 20/MN);
-    ctx.lineTo(xc, yc - 20/MN);
+    ctx.moveTo(xc, yc + 20);
+    ctx.lineTo(xc, yc - 20);
     ctx.stroke();
     for (var i=1; i<=(MaxX / step); i ++){
         ctx.beginPath();
@@ -90,6 +97,7 @@ function draw() {
     rover.style.width = 200/MN + 'px';
     document.getElementById('U').innerHTML = 'U='+UNow;
     document.getElementById('X').innerHTML = 'X='+XNow;
+    //requestAnimationFrame(draw);
 }
 requestAnimationFrame(draw);
   
